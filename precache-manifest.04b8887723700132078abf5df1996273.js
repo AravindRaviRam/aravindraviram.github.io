dfd91b70b88732f581d8da7de197519e49a5662d
arav <aravindsapt@gmail.com>
2019-09-24 00:06:50 +0530

    Updates

---
+++ precache-manifest.04b8887723700132078abf5df1996273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b43b61f3e04fccebeca3eb7f0869c0f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "31db9e777629c32be07b",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f62ca9ccadaede35cc57",
+    "url": "/portfolio/static/js/2.136e832f.chunk.js"
+  },
+  {
+    "revision": "31db9e777629c32be07b",
+    "url": "/portfolio/static/js/main.929b1ab0.chunk.js"
+  },
+  {
+    "revision": "cd137e883d409db4287c",
+    "url": "/portfolio/static/js/runtime-main.1641c94d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
